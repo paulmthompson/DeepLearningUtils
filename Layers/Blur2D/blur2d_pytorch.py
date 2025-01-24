69346b761029b3e6c70dc1daaf38000289f07f4d
Paul Thompson <pmt10@case.edu>
2025-01-24 16:16:13 -0500

    fix blur2d for padding=same

--- Layers/Blur2D/blur2d_pytorch.py
+++ Layers/Blur2D/blur2d_pytorch.py
@@ -16,6 +16,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+import math
 
 class Blur2D(nn.Module):
     def __init__(self, kernel_size=5, stride=2, kernel_type="Rect", padding='same'):
@@ -52,15 +53,28 @@ class Blur2D(nn.Module):
         self.stride = stride
         self.padding = padding
 
+    def calc_same_pad(self, i: int, k: int, s: int) -> int:
+        return max((math.ceil(i / s) - 1) * s + (k - 1) * 1 + 1 - i, 0)
+
     def forward(self, x):
         
         batch_size, channels, height, width = x.size()
         kernel = self.kernel2d.repeat(channels, 1, 1, 1).to(x.device)
 
         if self.padding == 'same':
-            padding = self.kernel_size // 2
-        else:
+            pad_h = self.calc_same_pad(i=height, k=self.kernel2d.shape[2], s=self.stride)
+            pad_w = self.calc_same_pad(i=width, k=self.kernel2d.shape[3], s=self.stride)
+
+            print('pad_h:', pad_h)
+            print('pad_w:', pad_w)
+
+            if pad_h > 0 or pad_w > 0:
+                x = F.pad(
+                    x, [pad_w // 2, pad_w - pad_w // 2, pad_h // 2, pad_h - pad_h // 2]
+                )
             padding = 0
+        else:
+            padding = self.padding
 
         # Apply depthwise convolution for blurring
         blurred = F.conv2d(x, kernel, stride=self.stride, padding=padding, groups=channels)
