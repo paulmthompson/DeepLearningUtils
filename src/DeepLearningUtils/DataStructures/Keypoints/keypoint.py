cc49cbfb15bf5cbf5fbaad68451128be1debe467
Paul Thompson <pmt10@case.edu>
2025-05-16 12:50:48 -0400

    add keypoint datastructure

---
+++ src/DeepLearningUtils/DataStructures/Keypoints/keypoint.py
@@ -0,0 +1,118 @@
+import csv
+from collections import OrderedDict
+
+import numpy as np
+
+
+class Keypoint:
+    def __init__(self, image):
+        self.prob = np.max(image)
+        coords = np.argwhere(image == self.prob)
+        if coords.shape[0] > 1:
+            self.x = round(np.mean(coords[:, 0]))
+            self.y = round(np.mean(coords[:, 1]))
+        else:
+            self.x = coords[0, 0]
+            self.y = coords[0, 1]
+
+
+class VideoKeypoints:
+    def __init__(
+        self,
+        video_height,
+        video_width,
+    ):
+        self.keypoints = []
+        self.frames = []
+        self._video_height = video_height
+        self._video_width = video_width
+
+    def add_frames(
+        self,
+        video_labels,
+        frame_offset,
+    ):
+        """
+
+        Parameters
+        ----------
+        video_labels
+        frame_offset: int
+            This should indicate the value of the first frame in the video_labels
+            data structure relative to the beginning of the entire video. If video labels contains
+            frames 4, 5, 6, and 7, then this value should be 4.
+
+        Returns
+        -------
+
+        """
+        for i in range(0, video_labels.shape[0]):
+            this_keypoint = Keypoint(video_labels[i, :, :])
+            self.keypoints.append(this_keypoint)
+            self.frames.append(i + frame_offset)
+
+    def to_csv(
+        self,
+        csv_path,
+        scale_height=1.0,
+        scale_width=1.0,
+        threshold=0.0,
+        delimiter=" ",
+    ):
+        """
+
+        Parameters
+        ----------
+        csv_path: string
+
+        Returns
+        -------
+
+        """
+        with open(csv_path, "w", newline="") as csvfile:
+            writer = csv.writer(csvfile, delimiter=delimiter)
+            writer.writerow(
+                [
+                    "Frame",
+                    "X",
+                    "Y",
+                    "Probability",
+                ]
+            )
+            for i in range(0, len(self.frames)):
+                if self.keypoints[i].prob < threshold:
+                    continue
+                writer.writerow(
+                    [
+                        self.frames[i],
+                        self.keypoints[i].x * scale_width,
+                        self.keypoints[i].y * scale_height,
+                        str(round(self.keypoints[i].prob, 2)),
+                    ]
+                )
+
+
+def read_keypoint_csv(
+        csv_path,
+        delimiter=" ",):
+    keypoint_list = []
+
+    csvfile = open(csv_path, newline="")
+    reader = csv.reader(csvfile, delimiter=delimiter)
+    for row in reader:
+        keypoint_list.append(row)
+
+    print(f"Read {len(keypoint_list)} keypoints from {csv_path}")
+
+    keypoint_list = keypoint_list[1:]  # Remove header
+
+    keypoint_coordinates = OrderedDict()
+
+    for i in range(len(keypoint_list)):
+        frame = keypoint_list[i][0]
+        keypoint_coordinates[int(frame)] = [
+            round(float(keypoint_list[i][2])),
+            round(float(keypoint_list[i][1])),
+        ]
+
+    return keypoint_coordinates
